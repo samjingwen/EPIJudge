d09e9b765cf3a0634e506459888cf671297b1ca9
samjingwen <samjingwen@u.nus.edu>
2024-10-19 13:28:42 +0800

    add solutions for 5.5

--- epi_judge_cpp/primitive_multiply.cc
+++ epi_judge_cpp/primitive_multiply.cc
@@ -1,13 +1,33 @@
 #include "test_framework/generic_test.h"
-unsigned long long Multiply(unsigned long long x, unsigned long long y) {
-  // TODO - you fill in here.
-  return 0;
+
+typedef unsigned long long ull;
+
+ull Add(ull a, ull b) {
+    ull sum = 0, carryin = 0, k = 1, temp_a = a, temp_b = b;
+    while (temp_a || temp_b) {
+        ull ak = a & k, bk = b & k;
+        ull carryout = (ak & bk) | (ak & carryin) | (bk & carryin);
+        sum |= (ak ^ bk ^ carryin);
+        carryin = (carryout << 1), temp_a >>= 1, temp_b >>= 1, k <<= 1;
+    }
+    return sum | carryin;
 }
 
-int main(int argc, char* argv[]) {
-  std::vector<std::string> args{argv + 1, argv + argc};
-  std::vector<std::string> param_names{"x", "y"};
-  return GenericTestMain(args, "primitive_multiply.cc",
-                         "primitive_multiply.tsv", &Multiply,
-                         DefaultComparator{}, param_names);
+ull Multiply(ull x, ull y) {
+    ull sum = 0;
+    while (x) {
+        if (x & 1) {
+            sum = Add(sum, y);
+        }
+        x >>= 1, y <<= 1;
+    }
+    return sum;
+}
+
+int main(int argc, char *argv[]) {
+    std::vector<std::string> args{argv + 1, argv + argc};
+    std::vector<std::string> param_names{"x", "y"};
+    return GenericTestMain(args, "primitive_multiply.cc",
+                           "primitive_multiply.tsv", &Multiply,
+                           DefaultComparator{}, param_names);
 }
