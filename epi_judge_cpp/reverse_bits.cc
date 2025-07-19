a4b495744aedd509bd69c5069e053279a936bb8c
samjingwen <samjingwen@u.nus.edu>
2025-07-19 16:10:33 +0800

    update solution for reverse_bits

--- epi_judge_cpp/reverse_bits.cc
+++ epi_judge_cpp/reverse_bits.cc
@@ -1,38 +1,50 @@
 #include "test_framework/generic_test.h"
 
-uint16_t reverseBits(uint16_t num) {
-    uint16_t result = 0;
-    for (int i = 0; i < 16; ++i) {
-        result <<= 1;
-        result |= (num & 1);
-        num >>= 1;
-    }
-    return result;
+constexpr uint8_t swapBits(uint8_t x, int i, int j) {
+  if ((x >> i & 1) != (x >> j & 1)) {
+    uint8_t mask = (1 << i) | (1 << j);
+    x ^= mask;
+  }
+
+  return x;
 }
 
-std::vector<uint16_t> generateReverseBitTable() {
-    std::vector<uint16_t> result(1 << 16);
-    for (uint32_t i = 0; i < (1 << 16); ++i) {
-        result[i] = reverseBits(i);
-    }
-    return result;
+constexpr unsigned long long reverseBits(uint8_t num) {
+  for (int i = 0; i < 4; ++i) {
+    num = swapBits(num, i, 7 - i);
+  }
+  return num;
 }
 
-static const std::vector<uint16_t> precomputed_reverse = generateReverseBitTable();
+constexpr std::array<unsigned long long, 1 << 8> generateReverseBitTable() {
+  std::array<unsigned long long, 1 << 8> table{};
+  for (uint32_t i = 0; i < (1 << 8); ++i) {
+    table[i] = reverseBits(i);
+  }
+  return table;
+}
+
+constexpr auto precomputed_reverse = generateReverseBitTable();
 
 unsigned long long ReverseBits(unsigned long long x) {
-    const int kWordSize = 16;
-    const int kBitMask = 0xFFFF;
+  const int kWordSize = 8;
+  const int kBitMask = 0xFF;
+  const int kChunks = 64 / kWordSize;
 
-    return static_cast<unsigned long long>(precomputed_reverse[x & kBitMask]) << (3 * kWordSize) |
-           static_cast<unsigned long long>(precomputed_reverse[(x >> kWordSize) & kBitMask]) << (2 * kWordSize) |
-           static_cast<unsigned long long>(precomputed_reverse[(x >> (2 * kWordSize)) & kBitMask]) << (1 * kWordSize) |
-           static_cast<unsigned long long>(precomputed_reverse[(x >> (3 * kWordSize)) & kBitMask]);
+  auto result = precomputed_reverse[x & kBitMask]
+                << ((kChunks - 1) * kWordSize);
+
+  for (int i = 1; i < kChunks; ++i) {
+    result |= (precomputed_reverse[(x >> (i * kWordSize)) & kBitMask]
+               << ((kChunks - 1 - i) * kWordSize));
+  }
+
+  return result;
 }
 
 int main(int argc, char *argv[]) {
-    std::vector<std::string> args{argv + 1, argv + argc};
-    std::vector<std::string> param_names{"x"};
-    return GenericTestMain(args, "reverse_bits.cc", "reverse_bits.tsv",
-                           &ReverseBits, DefaultComparator{}, param_names);
+  std::vector<std::string> args{argv + 1, argv + argc};
+  std::vector<std::string> param_names{"x"};
+  return GenericTestMain(args, "reverse_bits.cc", "reverse_bits.tsv",
+                         &ReverseBits, DefaultComparator{}, param_names);
 }
