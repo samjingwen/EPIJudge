9e9aa487fd6b9b30807c4011589d82f424ff374e
samjingwen <samjingwen@u.nus.edu>
2025-07-19 15:36:42 +0800

    update solution for swap_bits

--- epi_judge_cpp/swap_bits.cc
+++ epi_judge_cpp/swap_bits.cc
@@ -1,17 +1,17 @@
 #include "test_framework/generic_test.h"
 
 long long SwapBits(long long x, int i, int j) {
-    if ((x >> i & 1) != (x >> j & 1)) {
-        unsigned long long bit_mask = (1LL << i) | (1LL << j);
-        x ^= bit_mask;
-    }
+  if ((x >> i & 1) != (x >> j & 1)) {
+    auto bit_mask = (1ULL << i) | (1ULL << j);
+    x ^= bit_mask;
+  }
 
-    return x;
+  return x;
 }
 
 int main(int argc, char *argv[]) {
-    std::vector<std::string> args{argv + 1, argv + argc};
-    std::vector<std::string> param_names{"x", "i", "j"};
-    return GenericTestMain(args, "swap_bits.cc", "swap_bits.tsv", &SwapBits,
-                           DefaultComparator{}, param_names);
+  std::vector<std::string> args{argv + 1, argv + argc};
+  std::vector<std::string> param_names{"x", "i", "j"};
+  return GenericTestMain(args, "swap_bits.cc", "swap_bits.tsv", &SwapBits,
+                         DefaultComparator{}, param_names);
 }
