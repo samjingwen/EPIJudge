b3175ab39258c49eb9b67a953a323a6746c33790
samjingwen <samjingwen@u.nus.edu>
2025-07-19 11:52:01 +0800

    update solution for parity

--- epi_judge_cpp/parity.cc
+++ epi_judge_cpp/parity.cc
@@ -1,33 +1,35 @@
 #include "test_framework/generic_test.h"
 
-std::vector<uint16_t> generateParityTable() {
-    std::vector<uint16_t> result(1 << 16);
-    for (uint32_t i = 0; i < (1 << 16); ++i) {
-        uint32_t x = i;
+constexpr std::array<short, 1 << 8> generateParityTable() {
+  std::array<short, 1 << 8> table{};
+  for (uint16_t i = 0; i < table.size(); ++i) {
+    auto x = i;
 
-        short current = 0;
-        while (x) {
-            current ^= 1;
-            x &= (x - 1);
-        }
-        result[i] = current;
+    short current = 0;
+    while (x) {
+      current ^= 1;
+      x &= (x - 1);
     }
+    table[i] = current;
+  }
 
-    return result;
+  return table;
 }
 
-static const std::vector<uint16_t> precomputed_parity = generateParityTable();
+constexpr auto precomputed_parity = generateParityTable();
 
-short Parity(unsigned long long x) {
-    const int kWordSize = 16;
-    const int kBitMask = 0xFFFF;
+short Parity(uint64_t x) {
+  const int kWordSize = 8;
+  const int kBitMask = (1 << kWordSize) - 1;
+  const int kChunks = 64 / kWordSize;
 
-    return static_cast<short>(
-            precomputed_parity[x & kBitMask] ^
-            precomputed_parity[(x >> (1 * kWordSize)) & kBitMask] ^
-            precomputed_parity[(x >> (2 * kWordSize)) & kBitMask] ^
-            precomputed_parity[(x >> (3 * kWordSize)) & kBitMask]
-    );
+  short result = precomputed_parity[x & kBitMask];
+
+  for (int i = 1; i < kChunks; ++i) {
+    result ^= precomputed_parity[(x >> (i * kWordSize)) & kBitMask];
+  }
+
+  return result;
 }
 
 int main(int argc, char* argv[]) {
