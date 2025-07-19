d9f35cf678f922f083be1b85371db0255fa32255
samjingwen <samjingwen@u.nus.edu>
2025-07-19 18:24:19 +0800

    add solution for is_number_palindromic

--- epi_judge_cpp/is_number_palindromic.cc
+++ epi_judge_cpp/is_number_palindromic.cc
@@ -1,6 +1,28 @@
+#include <cmath>
+
 #include "test_framework/generic_test.h"
+
 bool IsPalindromeNumber(int x) {
-  // TODO - you fill in here.
+  if (x < 0) {
+    return false;
+  }
+
+  int size = std::floor(std::log10(x)) + 1;
+  int msd_mask = static_cast<int>(std::pow(10, size - 1));
+
+  for (int i = 0; i < size / 2; ++i) {
+    int msd = x / msd_mask;
+    int lsd = x % 10;
+
+    if (msd != lsd) {
+      return false;
+    }
+
+    x %= msd_mask;
+    x /= 10;
+    msd_mask /= 100;
+  }
+
   return true;
 }
 
--- problem_mapping.js
+++ problem_mapping.js
@@ -128,7 +128,7 @@ problem_mapping = {
         },
         "4.09 Check if a decimal integer is a palindrome": {
             "C++: is_number_palindromic.cc": {
-                "passed": 0,
+                "passed": 20000,
                 "total": 20000
             },
             "Java: IsNumberPalindromic.java": {
